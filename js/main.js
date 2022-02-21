ddbab3be55b289329ae3c7a4e85b538b8371cf17
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-02-21 17:04:26 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+import QrScanner from "../plugins/qrScanner/qrScanner.min.js";
+
+document.getElementById("timestamp").textContent = Date().toString();
+let srNumber = 1;
+
+function setQRDetail(qrData)
+{
+    const rowData = "<tr><td>" + ++srNumber + "</td><td>" + qrData + "</td><td>" + Date().toString() + "</td></tr>";
+    document.getElementsByTagName("tbody")[0].innerHTML += rowData;
+}
+
+function qrAlert()
+{
+    new Audio("../sounds/BarcodeScannerBeepSound.mp3").play();
+    navigator.vibrate(200);
+}
+    
+const qrScanner = new QrScanner
+(
+    document.getElementById("qrVideoInput"),
+    (result) =>
+    {
+        setQRDetail(result.data);
+        qrAlert();
+    },
+    {
+        highlightScanRegion: true,
+        highlightCodeOutline: true,
+        maxScansPerSecond: 1
+    }
+);
+
+qrScanner.setInversionMode("both");
+qrScanner.start();
